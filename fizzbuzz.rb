00d77dbefdda03d190e12adfaec3b6b73287e500
Kento Sato <tatehito.st@gmail.com>
2019-05-26 23:57:36 +0900

    Add test.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,31 @@
 def fizz_buzz
-    ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14",]
+    ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 end
 
 # 以下テストコード
 
 describe "fizz_buzz" do
 
-    it "3の倍数の代わりに”Fizz”を出力すること" do
+    it "3の倍数の代わりに”Fizz”を出力すること（ただし15の倍数を除く）" do
         fizz_buzz.each_with_index do |n, index|
-            if (index + 1) % 3 == 0
+            if (index + 1) % 3 == 0 && (index + 1) % 15 != 0
                 expect(n).to eq "Fizz"
             end
         end
     end
 
-    it "5の倍数の代わりに”Buzz”を出力すること" do
+    it "5の倍数の代わりに”Buzz”を出力すること（ただし15の倍数を除く）" do
         fizz_buzz.each_with_index do |n, index|
-            if (index + 1) % 5 == 0
+            if (index + 1) % 5 == 0 && (index + 1) % 15 != 0
                 expect(n).to eq "Buzz"
+            end
+        end
+    end
+
+    it "15の倍数の代わりに”FizzBuzz”を出力すること" do
+        fizz_buzz.each_with_index do |n, index|
+            if (index + 1) % 15 == 0
+                expect(n).to eq "FizzBuzz"
             end
         end
     end
