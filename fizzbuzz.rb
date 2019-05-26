158f88144360f45048344b00bf59011d13b49724
Kento Sato <tatehito.st@gmail.com>
2019-05-27 00:04:03 +0900

    Update fizz_buzz.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,25 @@
-def fizz_buzz
-    ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+def fizz_buzz(max)
+    (1..max).map do |n|
+        if n % 15 == 0
+            "FizzBuzz"
+        elsif n % 5 == 0
+            "Buzz"
+        elsif n % 3 == 0
+            "Fizz"
+        else
+            n.to_s
+        end
+    end
 end
 
 # 以下テストコード
 
 describe "fizz_buzz" do
 
+    result = fizz_buzz(1000)
+
     it "3の倍数の代わりに”Fizz”を出力すること（ただし15の倍数を除く）" do
-        fizz_buzz.each_with_index do |n, index|
+        result.each_with_index do |n, index|
             if (index + 1) % 3 == 0 && (index + 1) % 15 != 0
                 expect(n).to eq "Fizz"
             end
@@ -15,7 +27,7 @@ describe "fizz_buzz" do
     end
 
     it "5の倍数の代わりに”Buzz”を出力すること（ただし15の倍数を除く）" do
-        fizz_buzz.each_with_index do |n, index|
+        result.each_with_index do |n, index|
             if (index + 1) % 5 == 0 && (index + 1) % 15 != 0
                 expect(n).to eq "Buzz"
             end
@@ -23,7 +35,7 @@ describe "fizz_buzz" do
     end
 
     it "15の倍数の代わりに”FizzBuzz”を出力すること" do
-        fizz_buzz.each_with_index do |n, index|
+        result.each_with_index do |n, index|
             if (index + 1) % 15 == 0
                 expect(n).to eq "FizzBuzz"
             end
@@ -31,7 +43,7 @@ describe "fizz_buzz" do
     end
 
     it "3と5の倍数以外は数字を出力すること" do
-        fizz_buzz.each do |n|
+        result.each do |n|
             if n.class == Integer
                 expect(n % 3).to_not eq 0
                 expect(n % 5).to_not eq 0
