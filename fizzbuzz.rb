68f136c199f4b59eea7fc461fe15b5b1ed104684
Kento Sato <tatehito.st@gmail.com>
2019-05-26 23:01:46 +0900

    Add fizz_buzz.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,24 +1,25 @@
+def fizz_buzz
+    [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz"]
+end
 
 # 以下テストコード
 
 describe "" do
 
-    result = [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz"]
-
     it "3の倍数の代わりに”Fizz”を出力すること" do
-        2.step(result.size,3).each do |n|
-            expect(result[n]).to eq "Fizz"
+        2.step(fizz_buzz.size,3).each do |n|
+            expect(fizz_buzz[n]).to eq "Fizz"
         end
     end
 
     it "5の倍数の代わりに”Buzz”を出力すること" do
-        4.step(result.size,5).each do |n|
-            expect(result[n]).to eq "Buzz"
+        4.step(fizz_buzz.size,5).each do |n|
+            expect(fizz_buzz[n]).to eq "Buzz"
         end
     end
 
     it "3と5の倍数以外は数字を出力すること" do
-        result.each do |n|
+        fizz_buzz.each do |n|
             if n.class == Integer
                 expect(n % 3).to_not eq 0
                 expect(n % 5).to_not eq 0
